1b22446c2445d6bee34dbd1d42d036268ba2aad9
Sitaram <meenasitu0104@gmail.com>
2021-02-13 15:07:02 +0530

    baseurl

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.Node_ENV === "production"
-    ? "https://myStore2021.vercel.app"
+    ? "https://mystore2021.vercel.app/"
     : "http://localhost:3000";
 
 export default baseUrl;
